5e520cdcd679b417f7dc77e5708ed8fcab78c422
Melford Dike <melford.deekay@gmail.com>
2021-07-15 09:44:39 +0100

    dx updates 11

--- dxtrades/settings.py
+++ dxtrades/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'dxtrades.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
