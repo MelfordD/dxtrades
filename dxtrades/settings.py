3d0aeeddd48e2e99f5789cc22f1488130559c9c8
Melford Dike <melford.deekay@gmail.com>
2021-07-16 17:26:15 +0100

    dx updates 14

--- dxtrades/settings.py
+++ dxtrades/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 )
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dxtradeinvestment@gmail.com'
 EMAIL_HOST_PASSWORD = 'Blockchain1'
