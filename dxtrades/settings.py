316f654c0cdedc4247aa189bd807597107fe1508
MelfordD <melford.deekay@gmail.com>
2021-09-13 13:40:55 -0700

    change settings

--- dxtrades/settings.py
+++ dxtrades/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9im9v8!krhqn@%(w$$gwqfgwfhlx0^yg1d02yzb7-w5vkv-s_^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'dxmain',
     'accounts',
     'Balance',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +89,9 @@ DATABASES = {
         'PORT':  [5432]
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
