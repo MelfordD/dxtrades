5277408715bebb251ab37585b48e4bd06f88efcf
dkonti <richardmasondgreat@gmail.com>
2022-04-04 06:54:09 +0100

    static issues fix

--- dxtrades/settings.py
+++ dxtrades/settings.py
@@ -146,15 +146,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'asset')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
